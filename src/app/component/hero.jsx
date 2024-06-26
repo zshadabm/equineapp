3cce93eea3255e83eb07e6b2a06758bc0ebf91cd
zshadabm@gmail.com <zshadabm@gmail.com>
2024-06-26 22:45:52 +0300

    committed

--- src/app/component/hero.jsx
+++ src/app/component/hero.jsx
@@ -33,7 +33,7 @@ const Hero = () => {
 
             <div class="absolute rounded-full bg-fuchsia-300 -bottom-24 right-20 w-72 h-72 mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
             <div class="relative ">
-              <img class="object-cover object-center mx-auto rounded-lg shadow-2xl " alt="hero" src="/asset/image/hero.jpg"/>
+              <img class="object-cover object-center mx-auto rounded-lg shadow-2xl " alt="hero" src="./asset/image/hero.jpg"/>
             </div>
           </div>
         </div>
