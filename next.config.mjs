5c1058c2bc8c29092e889695f906df07818b1edd
zshadabm@gmail.com <zshadabm@gmail.com>
2024-06-26 22:38:10 +0300

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",  // <=== enables static exports
+    output: "export"  // <=== enables static exports
 };
 
 export default nextConfig;
